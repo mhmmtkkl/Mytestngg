5991245e188e8fe07c82c2b00abb01a73c38eb63
omeronal <omeronal@omers-mbp>
2019-08-10 15:43:32 -0400

    "asd"

--- src/test/java/Utilities/ReusableMethods.java
+++ src/test/java/Utilities/ReusableMethods.java
@@ -1,5 +1,28 @@
 package Utilities;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class ReusableMethods {
+	
+	private WebDriver driver;
+	
+	
+	public void SearchFunctionality(String myinput) {
+		
+		driver=myDriver.getDriver("Chrome");
+		
+		WebElement SearchingInput=  driver.findElement(By.xpath("//input[contains(@id,'search')]"));
+		
+		SearchingInput.sendKeys(myinput);
+		
+		WebElement SearchingClick = driver.findElement(By.xpath("//button[@type='submit']"));
+		
+		SearchingClick.click();
+		
+		
+		
+	}
 
 }
